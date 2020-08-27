86908fad59b629877d25d112862825e4c2cfc6ba
JaikoSuno <juan.quispe06@gmail.com>
2020-08-26 22:16:40 -0400

    FIX

--- GuiarSeguros/settings.py
+++ GuiarSeguros/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'z)kc&7h%(e7!_cviegm*szw=&n5*8%zg#^xzlmfjmb3ffqenl^'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
