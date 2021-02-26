d398d5261d4666e5ffbe435114f739d2375895ea
Manuel <manuelzuletab@gmail.com>
2021-02-25 21:59:52 -0300

    subida a produccion

--- GuiarSeguros/settings.py
+++ GuiarSeguros/settings.py
@@ -137,4 +137,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'manuelzuletab@gmail.com'
-EMAIL_HOST_PASSWORD = 'andreitazb/25'
+EMAIL_HOST_PASSWORD = 'andreitazb/25max94'
